92836934277d6d7409abcecc305820f47e1b9d19
STUDENT Rani Wheeza <Rani.Wheeza@students.ittralee.ie>
2023-11-30 11:07:20 +0000

    Signed-off-by: STUDENT Rani Wheeza <Rani.Wheeza@students.ittralee.ie>

---
+++ My first game/Assets/Bomb.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            explode();
+        }
+
+
+    }
+
+
+    private void explode()
+    {
+        Collider[] victims = Physics.OverlapSphere(transform.position, 10);
+
+        foreach (Collider victim in victims)
+        {
+            Rigidbody rb_victim = victim.GetComponent<Rigidbody>();
+            if (rb_victim != null)
+            {
+                rb_victim.AddExplosionForce(2000, transform.position, 10);
+            }
+
+
+        }
+    }
+
+}
