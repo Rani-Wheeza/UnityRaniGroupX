c052099e633598ee0178e71f40c5355426eae07d
STUDENT Rani Wheeza <Rani.Wheeza@students.ittralee.ie>
2023-12-07 11:01:52 +0000

    Signed-off-by: STUDENT Rani Wheeza <Rani.Wheeza@students.ittralee.ie>

--- My first game/Assets/Eddcontrollscript.cs
+++ My first game/Assets/Eddcontrollscript.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Eddcontrollscript : MonoBehaviour
 {
     Animator edAnimations;
-
+    float speed = 3f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,21 @@ public class Eddcontrollscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        edAnimations.SetBool("shouldjump", true);
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += speed * transform.forward * Time.deltaTime;
+            edAnimations.SetBool("shouldWalk", true);
+        }
+        else
+            edAnimations.SetBool("shouldWalk", false);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            
+            edAnimations.SetBool("shouldjump", true);
+        }
+        else 
+            edAnimations.SetBool("shouldjump", false);
+
     }
 }
